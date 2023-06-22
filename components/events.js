731faaf26d2a9f4cbe031fdb8a10d8f6a7e537c6
Sarthak Mohanty <sarthaktexas@gmail.com>
2023-06-22 11:35:48 -0400

    event githubs now open in a new tab

--- components/events.js
+++ components/events.js
@@ -34,7 +34,7 @@ export default function Events () {
                     <img className="w-1/3" src={logo} alt={`Logo for ${name}`} />
                     <p className="text-lg italic">{location}</p>
                     <p className="">{description}</p>
-                    <a href={github_link} className="underline text-lg text-gray-500 hover:text-[#ec3750] pb-2">See what went into planning
+                    <a href={github_link} target="_blank" className="underline text-lg text-gray-500 hover:text-[#ec3750] pb-2">See what went into planning
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="pl-2 h-5 inline">
                         <path fillRule="evenodd" d="M4.25 5.5a.75.75 0 00-.75.75v8.5c0 .414.336.75.75.75h8.5a.75.75 0 00.75-.75v-4a.75.75 0 011.5 0v4A2.25 2.25 0 0112.75 17h-8.5A2.25 2.25 0 012 14.75v-8.5A2.25 2.25 0 014.25 4h5a.75.75 0 010 1.5h-5z" clipRule="evenodd" />
                         <path fillRule="evenodd" d="M6.194 12.753a.75.75 0 001.06.053L16.5 4.44v2.81a.75.75 0 001.5 0v-4.5a.75.75 0 00-.75-.75h-4.5a.75.75 0 000 1.5h2.553l-9.056 8.194a.75.75 0 00-.053 1.06z" clipRule="evenodd" />
