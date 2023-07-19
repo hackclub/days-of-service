b8d7f9c34f5fb2b09346f846b87dbc83bdf3ef99
Sarthak Mohanty <sarthaktexas@gmail.com>
2023-07-19 09:21:03 -0400

    fix head component

--- components/head.js
+++ components/head.js
@@ -3,12 +3,12 @@ import Head from 'next/head'
 export default function HeadObject({children}) {
     const title = "Day of Service - Hack Club";
     const description = "All the info about Hack Club's Day of Service initiative";
-    const searchBarColor = "#ffffff"; // This is your Safari 15 Search Bar Color in Light Mode
+    // const searchBarColor = "#ffffff"; // This is your Safari 15 Search Bar Color in Light Mode
     //const darkSearchBarColor = "#000000"; // This is your Safari 15 Search Bar Color in Dark Mode (optional)
     const keywords = "hackclub";
     const author = "Hack Club";
     const twitter = "@starthackclub";
-    const url = "http://localhost:3000"; // This is your og:url or domain (optional but recommended)
+    // const url = "http://localhost:3000"; // This is your og:url or domain (optional but recommended)
     //const image = "/ogimage.png"; // This is your OpenGraph image
     return (
         <Head>
@@ -19,7 +19,7 @@ export default function HeadObject({children}) {
             <meta name="description" content={description} />
             <meta name="keywords" content={keywords} />
             <meta name="author" content={author} />
-            <meta name="theme-color" content={searchBarColor} media="(prefers-color-scheme: light)" />
+            {/* <meta name="theme-color" content={searchBarColor} media="(prefers-color-scheme: light)" /> */}
             {/* <meta name="theme-color" content={darkSearchBarColor} media="(prefers-color-scheme: dark)" /> */}
             {url ? <meta property="og:url" content={url} /> : ''}
             <meta property="og:type" content="website" />
