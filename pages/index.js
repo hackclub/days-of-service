204ce2c6e131b844ca01b03e41861665e3ef8b89
nilaRam <58853644+nilaRam@users.noreply.github.com>
2024-03-15 11:24:19 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -96,6 +96,7 @@ export default function Home() {
             </quote>
             — Anna Marie Leidelmeijer
               <br /> Program Specialist, Girl Scouts of Greater LA
+          </section>
           <section className="flex flex-col gap-2 px-4 lg:px-10 relative">
             <quote className="text-lg before:content-[''] before:absolute before:bg-cover before:-top-2 before:left-6 before:opacity-40 before:z-0 before:w-7 before:h-7 before:bg-[url('https://icons.hackclub.com/api/icons/0xec3750/glyph:quote.svg')]">
               <br /> It was a magical day! We hope we can work with you again in the future.
